1c4ba27f9d223d9e580fd95ba7f93c9f899ad8c8
VVKozlov <vasyakus@gmail.com>
2023-07-05 11:21:54 +0300

    Add exceptions to Task Add try catch

--- Homework_1/src/Task_4.java
+++ Homework_1/src/Task_4.java
@@ -1,3 +1,31 @@
-public class Task_4 {
+import java.util.Arrays;
 
+public class Task_4 {
+    public static void main(String[] args) {
+        Integer[] arr1 = {1,2,3,4,5};
+        Integer[] arr2 = {6,7,0,9,10};
+        try {
+            System.out.println(Arrays.toString(arrDivider(arr1, arr2)));
+        }
+        catch (RuntimeException e) {
+            System.err.println(e.getMessage());
+        }
+
+    }
+    public static Integer[] arrDivider (Integer[] array1, Integer[] array2){
+        if (array2.length != array1.length) {
+            throw new RuntimeException("Invalid array length!");
+        }
+        Integer[] res =  new Integer[array1.length];
+        for (int i = 0; i < array2.length; i++){
+            if (array2[i] == 0) {
+                throw new RuntimeException("Invalid divider!\n" +
+                        "Element " + i + " of array2 is 0");
+            }
+            else {
+                res[i] = array1[i]/array2[i];
+            }
+        }
+        return res;
+    }
 }
