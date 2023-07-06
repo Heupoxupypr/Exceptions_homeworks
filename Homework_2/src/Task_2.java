b699cda309036c6875fe40cf9fd253014911a3c0
VVKozlov <vasyakus@gmail.com>
2023-07-06 08:42:28 +0300

    Add code into Task 2

--- Homework_2/src/Task_2.java
+++ Homework_2/src/Task_2.java
@@ -1,5 +1,16 @@
 public class Task_2 {
     public static void main(String[] args) {
-
+        int[] intArray = {9,8,7,6,5,4,3,2,1};
+        int d = 0;
+        if (intArray.length < 9) {
+            System.out.println("Error! Array length is less than 9");
+        }
+        else if (d ==0) {
+            System.out.println("Error! d equals 0");
+        }
+        else {
+            double catchedRes1 = intArray[8] / d;
+            System.out.println("catchedRes1 = " + catchedRes1);
+        }
     }
 }
