4a971be9a03f00bccd441c59483ae7ed332cc83b
VVKozlov <vasyakus@gmail.com>
2023-07-06 09:31:34 +0300

    Add code into Task 4

--- Homework_2/src/Task_4.java
+++ Homework_2/src/Task_4.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class Task_4 {
     public static void main(String[] args) {
-        
+        Scanner in = new Scanner(System.in);
+        System.out.println("Please enter string:");
+        String line = in.nextLine();
+        if (line == null || line.isEmpty()){
+            throw new RuntimeException("String is empty!");
+        }
+        else {
+            System.out.println("Entered string: " + line);
+        }
     }
 }
