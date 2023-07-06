7acb0edce02e5650d3e744ce328a8cf8990162d3
VVKozlov <vasyakus@gmail.com>
2023-07-06 08:20:45 +0300

    Add code into Task 1. Add method inputNumber

--- Homework_2/src/Task_1.java
+++ Homework_2/src/Task_1.java
@@ -1,5 +1,23 @@
+import java.io.IOException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Task_1 {
     public static void main(String[] args) {
+        float num = inputNumber();
+        System.out.printf("Entered number %.2f\n",num);
 
+    }
+
+    static float inputNumber(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter fractional number:");
+//        input.nextLine();
+        while (!input.hasNextFloat()){
+            System.out.println("This is not number! Please repeat!");
+            System.out.println("Please enter fractional number:");
+            input.nextLine();
+        }
+        return input.nextFloat();
     }
 }
