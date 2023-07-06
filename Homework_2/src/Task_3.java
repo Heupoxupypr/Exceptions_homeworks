28b785823cb23ccbcd4062fcc5bd010a16de5c51
VVKozlov <vasyakus@gmail.com>
2023-07-06 09:12:26 +0300

    Add code into Task 3

--- Homework_2/src/Task_3.java
+++ Homework_2/src/Task_3.java
@@ -1,5 +1,24 @@
 public class Task_3 {
     public static void main(String[] args) {
 
+        int a = 90;
+        int b = 0;
+        try {
+            System.out.println(a / b);
+        } catch (ArithmeticException ex) {
+            System.out.println("Нельзя делить на 0!!!");
+        }
+        printSum(23, 234);
+        try {
+            int[] abc = { 1, 2 };
+            abc[3] = 9;
+        }
+        catch (IndexOutOfBoundsException ex) {
+            System.out.println("Элемента с таким порядковым номером нет в массиве!!");
+        }
     }
+    public static void printSum(Integer a, Integer b) {
+        System.out.println(a + b);
+    }
+
 }
